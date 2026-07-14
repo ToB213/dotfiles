18dbba849e978f3202e90ef64c6893c3a457f9ef
ToB213 <niimicomputergraphics@gmail.com>
2026-07-14 11:29:26 +0900

    🐛 bassの補助ファイルの追加忘れを修正

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -24,6 +24,7 @@ in
     ".config/fish/conf.d".source = outOfStoreSymlink "${dotfilesRoot}/config/fish/conf.d";
     ".config/fish/functions/y.fish".source = outOfStoreSymlink "${dotfilesRoot}/config/fish/functions/y.fish";
     ".config/fish/functions/bass.fish".source = "${bass}/share/fish/vendor_functions.d/bass.fish";
+    ".config/fish/functions/__bass.py".source = "${bass}/share/fish/vendor_functions.d/__bass.py";
   }
   // builtins.listToAttrs (
     map (name: {
